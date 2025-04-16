c5e8beabfd29fba07d7aa7dc2c0bce877e9a1e38
adrianakbar <armojkece123@gmail.com>
2025-04-16 14:06:11 +0700

    [REFAC] Update class names in Navbar component for improved consistency in styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -168,7 +168,7 @@ export default function Navbar({ navbarItems }: { navbarItems: NavbarItem[] }) {
                     onItemClick={closeUserDropdown}
                     tag="a"
                     href="/profile"
-                    className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
+                    className="flex items-center gap-3 px-3 py-2 text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
                   >
                     <svg
                       className="fill-gray-500 group-hover:fill-gray-700 dark:fill-gray-400 dark:group-hover:fill-gray-300"
