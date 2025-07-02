f966fb2eb003b6979b7296110da55fa8b74ea60b
adrianakbar <armojkece123@gmail.com>
2025-07-02 19:59:02 +0700

    [FEAT] Implement ClientLayout component for improved layout structure and navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,20 @@
-"use client";
-
+import ClientLayout from "./components/ClientLayout";
 import "./globals.css";
-import { usePathname } from "next/navigation";
-import Navbar from "./components/Navbar";
-import { Building, CalendarCheck, Coffee, House } from "lucide-react";
-import Head from "next/head";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Sekolah Kopi Raisa',
+  description: 'Sistem informasi untuk mengelola kegiatan dan produk',
+  icons: {
+    icon: '/assets/logo.png',
+  },
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-
-  const isAuthPage = pathname === "/login" || pathname === "/signup" || pathname === "/reset-password";
-  const isAdminPage = pathname.startsWith("/admin");
-
-  const navbarItems = [
-    { title: "Beranda", link: "/", icon: <House size={20} /> },
-    { title: "Tentang", link: "/about", icon: <Building size={20} /> },
-    { title: "Produk", link: "/product", icon: <Coffee size={20} /> },
-    { title: "Kegiatan", link: "/activity", icon: <CalendarCheck size={20} /> },
-  ];
-
   return (
     <html lang="id">
-      <Head>
-        <title>Sekolah Kopi Raisa</title>
-        <meta name="description" content="Sistem informasi untuk mengelola kegiatan dan produk" />
-        <link rel="icon" href="/assets/logo.png" />
-      </Head>
       <body>
-        {!isAuthPage && !isAdminPage && <Navbar navbarItems={navbarItems} />}
-        <main>{children}</main>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
